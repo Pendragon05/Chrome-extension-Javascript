8e60262bc5ad329661e48bfbf8b211deee518764
James Baranuelo <122170760+Pendragon05@users.noreply.github.com>
2024-09-14 22:59:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 //   console.log("Button clicked!");
 // }
 
-let inputBtn = document.getElementById("input-btn");
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
 
 inputBtn.addEventListener("click", function () {
   console.log("Button clicked!");
