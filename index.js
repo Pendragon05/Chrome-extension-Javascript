e828e08307fd27740265d4f653e11c69d6770cb0
James Baranuelo <122170760+Pendragon05@users.noreply.github.com>
2024-09-23 23:51:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ inputBtn.addEventListener("click", function () {
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    ulEl.textContent += "<li>" + myLeads[i] + " ";
+    listItems +=
+      "<li><a target='_blank' href='" +
+      myLeads[i] +
+      "'>" +
+      myLeads[i] +
+      "</a></li>";
   }
   ulEl.innerHTML = listItems;
 }
